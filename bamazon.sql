ffb04cc31edd62a5ac9c30855b4b9fa682828b3d
Jessica VC <jessicavc@this-guy-ii.home>
2020-07-06 22:34:29 -0700

    command line table is populating via MySQL db. Item ID inquiry is working.

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,5 @@
+-- Drop bamazon_db in case it already exists
+DROP DATABASE bamazon_db;
 -- Create a MySQL Database called `bamazon`.
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
@@ -9,37 +11,37 @@ USE bamazon_db;
 -- price (cost to customer)
 -- stock_quantity (how much of the product is available in stores)
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  item_id VARCHAR (150) NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   # unique id for each product
-  product_name VARCHAR (150) NOT NULL,
+  product_name TEXT NOT NULL,
   # name of product
   department_name VARCHAR (150) NOT NULL,
-  price INT default 0,
+  price DECIMAL (10,2) NULL,
   # cost to customer
-  stock_quantity INT default 0,
+  stock_quantity INT NULL,
   # how much of the product is available in stores
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id)
 );
 -- Populate this database with around 10 different products.
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("HB123", "Hasbro GI Joe Action Figure", "Toys", 19.99, 25 ),
-( "H829", "Hammock Inflatable Pool", "Toys", 24.99, 10),
-( "TR001", "Transformers Optimus Prime Action Figure", "Toys", 49.99, 5),
-( "ED002", "Echo Dot", "Electronics", 29.00, 55),
-( "FT001","Fire Tablet", "Electronics", 49.99, 55),
-("R123", "Roku Media Player", "Electronics", 24.00, 5),
-("FM911", "Face Cover Mouth Mask", "Clothing", 12.95, 2),
-("BS209", "Biker Shorts", "Clothing", 19.99, 70),
-("WS002","Water Sport Shoes", "Clothing", 17.99, 50),
-("CC68", "Crocs Classic Clog", "Clothing", 19.99, 50),
-("POO009", "Earth Rated Dog Poop Bags", "Pet Supplies", 5.99, 155),
-("CPOO123","Dr Elsey’s Premier Clumping Cat Litter", "Pet Supplies", 39.00, 340),
-("DFT111", "Seresto Flea and Tick Collar for Dogs", "Pet Supplies", 42.98, 234),
-("H20XZ", "Sparkling Ice Black Cherry Sparkling Water", "Grocery", 34.99, 297),
-("0R30", "Nabisco Variety Pack", "Grocery", 6.32, 879),
-("BW123", "bubly Sparkling Water Variety Pack", "Grocery", 23.99, 134),
-("FM9112", "Face Mask case of 500", "Hygiene", 29.94, 1),
-("MMS255", "Mrs Meyers Cruelty Free Hand Soap", "Hygiene", 7.29, 793),
-("DBW123", "Dove Body Wash", "Hygiene", 18.99, 807);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hasbro GI Joe Action Figure", "Toys", 19.99, 25 ),
+( "Hammock Inflatable Pool", "Toys", 24.99, 10),
+( "Transformers Optimus Prime Action Figure", "Toys", 49.99, 5),
+( "Echo Dot", "Electronics", 29.00, 55),
+( "Fire Tablet", "Electronics", 49.99, 55),
+( "Roku Media Player", "Electronics", 24.00, 5),
+("Face Cover Mouth Mask", "Clothing", 12.95, 2),
+("Biker Shorts", "Clothing", 19.99, 70),
+("Water Sport Shoes", "Clothing", 17.99, 50),
+("Crocs Classic Clog", "Clothing", 19.99, 50),
+("Earth Rated Dog Poop Bags", "Pet Supplies", 5.99, 155),
+("Dr Elsey’s Premier Clumping Cat Litter", "Pet Supplies", 39.00, 340),
+("Seresto Flea and Tick Collar for Dogs", "Pet Supplies", 42.98, 234),
+("Sparkling Ice Black Cherry Sparkling Water", "Grocery", 34.99, 297),
+( "Nabisco Variety Pack", "Grocery", 6.32, 879),
+("bubly Sparkling Water Variety Pack", "Grocery", 23.99, 134),
+("Face Mask case of 500", "Hygiene", 29.94, 1),
+("Mrs Meyers Cruelty Free Hand Soap", "Hygiene", 7.29, 793),
+("Dove Body Wash", "Hygiene", 18.99, 807);
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var hide = require("hide-secrets");
+var Table = require("cli-table");
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -17,4 +20,62 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
+  //welcome message
+  console.log("Welcome to Bamazon!"); 
+  //start the app
+  displayItems();
 });
+
+// GLOBAL VARIABLES
+// =====================================================================================
+var chosenItem = {};
+
+// FUNCTIONS
+// =====================================================================================
+// function to reset the chosenItem array so that previous purchases are not inside
+var resetCart = function() {
+    chosenItem = {};
+}
+
+// function to display all items for sale
+var displayItems = function() {
+    connection.query(`SELECT * FROM products`, (err, res) => {
+        var listTable = new Table({
+            head: ['Item ID', 'Product Name', 'Price'],
+            colWidths: [10, 45, 12]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            listTable.push([res[i].item_id, res[i].product_name, `${res[i].price}`]);
+            
+        }
+        
+        console.log(`\n\n${listTable.toString()}\n\n`);
+        // ask user to enter ID of item they wish to purchase
+        askForID();
+    });
+};
+
+// function to prompt user to enter ID of the product to purchase
+var askForID = function() {
+    inquirer.prompt({
+        name: 'itemID',
+        type: 'input',
+        message: 'Enter the ID of the item you would like to purchase:',
+        // validate input 
+        validate: (value) => {
+            if (!isNaN(value) && (value > 0 && value <= 20)) {
+                return true;
+            } else {
+                console.log("Please enter a number from 1-20");
+                return false;
+            }
+        }
+    // select all rows where ID equals user input
+    }).then((answer) => {
+        connection.query('SELECT item_id, product_name, price, stock_quantity, product_sales FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
+            // confirm with user that this is the product they'd like to purchase
+            confirmItem(res[0].product_name, res);
+        });
+    });
+};
